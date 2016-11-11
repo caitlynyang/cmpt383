e77d3bc0d7ef43bfdf7eec19c3595edcdad71c5d
sengami-yuka <sengami.yuka@gmail.com>
2016-11-10 19:27:52 -0800

    Update proj2.scm

--- proj2/proj2.scm
+++ proj2/proj2.scm
@@ -1,9 +1,26 @@
+(define pets
+       '((cat 1) (dog 1) (fish 1) (cat 2) (fish 2))
+)
+
 (define is-pair?
 	(lambda (x)
 		(and
 			(list? x)
 			(not (equal? '() (cdr x)))
 			(equal? '() (cdr (cdr x)))
+		)
+	)
+)
+
+(define is-alist?
+	(lambda (x)
+		(or
+			(equal? '() x)
+			(and
+				(list? x)
+				(is-pair? (car x))
+				(is-alist? (cdr x))
+			)
 		)
 	)
 )
